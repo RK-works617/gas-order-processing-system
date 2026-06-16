cfa0b48be53d09e4e6dd105997d6affa89f7b3a3
RK-works617 <ryosuke.koide0327@gmail.com>
2026-06-16 16:50:04 +0900

    Update salesAggregationSystem.js

--- salesAggregationSystem.js
+++ salesAggregationSystem.js
@@ -1,1 +1,15 @@
-
+function dailySalesAggregation(){
+  let sheet3=SpreadsheetApp.openById("1liiKr_ivAYwlLtxrwH8S_OQL7UV5kUqL1G-XEtnzkRA").getSheetByName("売上管理表");
+  // 元のスタンドアロンスクリプトから派生しているので、変数名がsheet3となっている。
+  let last=sheet3.getLastRow();
+  let daily=0;
+  for (i=2; i<=last; i++){
+    if(sheet3.getRange(i,1).getDisplayValue()==sheet3.getRange(2,4).getDisplayValue()){
+      daily=daily+Number(sheet3.getRange(i,2).getValue());
+    }
+  }
+  sheet3.getRange(2,5).setValue(daily);
+  // console.log(i);
+  // console.log(sheet3.getRange(i,1).getDisplayValue());
+  // console.log(sheet3.getRange(2,4).getDisplayValue());
+}
